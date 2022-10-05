12eb2c75fb41277150efbdf07a0858f710297fb2
pizarrosch <bayernsch89@gmail.com>
2022-10-05 21:12:15 +0200

    Applying inline-styling

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+    setEnteredValue(event.target.value);
+    setIsValid(true);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if(enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue('');
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{border: !isValid ? '1px red solid' : null, backgroundColor: !isValid ? 'lightpink' : null}} value={enteredValue} type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
