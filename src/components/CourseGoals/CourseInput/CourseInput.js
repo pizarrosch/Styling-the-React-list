08692b9035b43e6ff4c6ef19f31d136f57dc62d1
pizarrosch <bayernsch89@gmail.com>
2022-10-08 20:57:55 +0200

    Applying css module

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,35 +1,35 @@
 import React, { useState } from 'react';
-
+import s from './CourseInput.module.css';
 import Button from '../../UI/Button/Button';
-// import './CourseInput.css';
+// import './CourseInput.module.css';
 import styled from "styled-components";
 
-const FormControl = styled.div`
-    margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${({notValid}) => (notValid ? 'red' : 'black')};
-  }
-  
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${({notValid}) => (notValid ? 'red' : 'blue')};
-    background-color: ${({notValid}) => (notValid ? 'lightpink' : null)};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: aliceblue;
-    border-color: cornflowerblue;
-  }
-`;
+// const FormControl = styled.div`
+//     margin: 0.5rem 0;
+//
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${({notValid}) => (notValid ? 'red' : 'black')};
+//   }
+//
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${({notValid}) => (notValid ? 'red' : 'blue')};
+//     background-color: ${({notValid}) => (notValid ? 'lightpink' : null)};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+//
+//   & input:focus {
+//     outline: none;
+//     background: aliceblue;
+//     border-color: cornflowerblue;
+//   }
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -52,10 +52,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl notValid={!isValid}>
+      <div className={`${s.formControl} ${!isValid ? s.invalid : s.formControl}`}>
         <label>Course Goal</label>
         <input value={enteredValue} type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
